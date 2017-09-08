22f9dee1fd5120ffd2b2bdcf40da441ffdab4a33
Sergey <sergejs.terentjevs@gmail.com>
2017-09-08 20:30:23 +0100

    Added math factors solution and Java tips

---
+++ tips/LiteralsDemo.java
@@ -0,0 +1,12 @@
+final class LiteralsDemo {
+
+    public static void run() {
+        int hex = 0xFF;
+        int oct = 011;
+        int bin = 0b1100;
+
+        System.out.println("Hex: " + hex);
+        System.out.println("Octal: " + oct);
+        System.out.println("Binary: "  + bin);
+    }
+}
