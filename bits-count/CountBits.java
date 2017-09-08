20245968eb2db534cf3aa6101a318041f23fa554
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2017-09-08 16:24:34 +0100

    Made bits count method public

--- bits-count/CountBits.java
+++ bits-count/CountBits.java
@@ -1,10 +1,10 @@
 public class CountBits {
     /**
-     * Counts 1 bit presence in given number.
+     * Counts bit '1' presence in given number.
      * @param value The number to inspect
-     * @return the count of 1 bit in a given number
+     * @return the count of bit '1' in a given number
      */
-    private static int countPopulation(int value) {
+    public static int countPopulation(int value) {
         int count = 0;
 
         while (value > 0) {
