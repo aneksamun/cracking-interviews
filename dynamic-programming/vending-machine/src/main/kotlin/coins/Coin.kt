d789ecb0dc7d1a9b9a3be1d2c3caae9d892abcce
Sergey <sergejs.terentjevs@gmail.com>
2017-09-10 03:37:46 +0100

    Removed invalid line

--- dynamic-programming/vending-machine/src/main/kotlin/coins/Coin.kt
+++ dynamic-programming/vending-machine/src/main/kotlin/coins/Coin.kt
@@ -3,8 +3,6 @@ package coins
 abstract class Coin protected constructor(val denomination: Int, val currency: Currency) : Comparable<Coin> {
 
     override fun compareTo(other: Coin): Int {
-        val p = Pack(FivePence(), 5)
-
         return this.denomination.compareTo(other.denomination)
     }
 }
