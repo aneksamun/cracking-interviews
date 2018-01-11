68ba4b080033aeaead4efe77b6d3cc5ebae9fb23
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2018-01-11 23:31:47 +0000

    temp

--- dynamic-programming/vending-machine/src/main/kotlin/cli/VendingHelpFormatter.kt
+++ dynamic-programming/vending-machine/src/main/kotlin/cli/VendingHelpFormatter.kt
@@ -2,12 +2,9 @@ package cli
 
 import com.xenomachina.argparser.HelpFormatter
 
-class VendingHelpFormatter : HelpFormatter {
+class UsageFormatter : HelpFormatter {
 
     override fun format(progName: String?, columns: Int, values: List<HelpFormatter.Value>): String {
-
-        return buildString {
-            append("usage: java -jar vending-machine-1.0.jar (--limited|--unlimited) [-h]")
-        }
+        return "usage: ${progName ?: ""} [-h] (--limited|unlimited)"
     }
 }
