860daee6986ada7e9a6007d2ad66d35d8b48b77d
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2018-05-13 01:54:32 +0100

    Added flags example

---
+++ tips/flags/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    using Flags;
+    using static Console;
+
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine(CurrencyResolver.Instance.Resolve(Currency.Gbp));
+        }
+    }
+}
