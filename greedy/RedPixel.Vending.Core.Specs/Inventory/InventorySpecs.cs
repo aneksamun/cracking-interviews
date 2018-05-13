bd32809e0c6583c1f85649cb5c04490e468de16c
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2018-05-13 01:01:17 +0100

    Added greedy coin change solution

---
+++ greedy/RedPixel.Vending.Core.Specs/Inventory/InventorySpecs.cs
@@ -0,0 +1,46 @@
+﻿namespace RedPixel.Vending.Core.Specs.Inventory
+{
+    using System.Diagnostics.CodeAnalysis;
+    using FluentAssertions;
+    using Machine.Specifications;
+    using Supply;
+    using static Supply.Denomination;
+    
+    [Subject(typeof(Inventory))]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class when_formatting_string
+    {
+        Establish context = () =>
+        {
+            inventory = new Inventory
+            {
+                Pack.Of(new Pence(One), 1),
+                Pack.Of(new Pence(Two), 2),
+                Pack.Of(new Pence(Five), 5),
+                Pack.Of(new Pence(Ten), 10),
+                Pack.Of(new Pence(Twenty), 20),
+                Pack.Of(new Pence(Fifty), 50),
+                Pack.Of(new Pound(One), 100),
+                Pack.Of(new Pound(Two), 200),
+                Pack.Of(new Pound(Three), 300),
+                Pack.Of(new Pound(Five), 500),
+                Pack.Of(new Pound(Ten), 1000),
+                Pack.Of(new Pound(Twenty), 2000),
+                Pack.Of(new Pound(Fifty), 5000)
+            };
+        };
+
+        Because of = () =>
+        {
+            actual = inventory.ToString();
+        };
+
+        It should_separate_units_by_comma = () =>
+        {
+            actual.Should().Be("1px1, 2px2, 5px5, 10px10, 20px20, 50px50, £1x100, £2x200, £5x500, £10x1000, £20x2000, £50x5000");
+        };
+
+        static Inventory inventory;
+        static string actual;
+    }
+}
---
+++ greedy/RedPixel.Vending.Core/Change.cs
@@ -0,0 +1,45 @@
+﻿namespace RedPixel.Vending.Core
+{
+    using System.Collections.Generic;
+    using System.Text;
+    using Supply;
+    using static System.Array;
+
+    public sealed class Change : Dictionary<Unit, int>
+    {
+        private const string Separator = ", ";
+
+        public void Merge(Unit unit)
+        {
+            if (TryGetValue(unit, out var value))
+            {
+                this[unit] = ++value;
+            }
+            else
+            {
+                Add(unit, ++value);   
+            }
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            foreach (var key in Keys)
+            {
+                var value = this[key];
+                builder.Append($"{key}x{value}{Separator}");
+            }
+
+            var str = builder.ToString();
+            return str.Remove(str.Length - Separator.Length, Separator.Length);
+        }
+
+        public static Change Of(params (Unit, int)[] pairs)
+        {
+            var change = new Change();
+            ForEach(pairs, pair => change[pair.Item1] = pair.Item2);
+            return change;
+        }
+    }
+}
