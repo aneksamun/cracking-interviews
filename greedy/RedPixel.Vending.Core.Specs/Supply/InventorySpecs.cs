f08ba9d99fa58902b5e6f605ea874621a62e0daf
Sergey Terentyev <sergey.terentyev@kindredgroup.com>
2018-12-28 17:43:57 +0000

    Adding more specs

--- greedy/RedPixel.Vending.Core.Specs/Supply/InventorySpecs.cs
+++ greedy/RedPixel.Vending.Core.Specs/Supply/InventorySpecs.cs
@@ -1,14 +1,14 @@
-﻿namespace RedPixel.Vending.Core.Specs.Inventory
+﻿namespace RedPixel.Vending.Core.Specs.Supply
 {
     using System.Diagnostics.CodeAnalysis;
+    using Core.Supply;
     using FluentAssertions;
     using Machine.Specifications;
-    using Supply;
-    using static Supply.Denomination;
+    using static Core.Supply.Denomination;
     
     [Subject(typeof(Inventory))]
     [SuppressMessage("ReSharper", "InconsistentNaming")]
-    public class when_formatting_string
+    public class when_converting_inventory_to_string
     {
         Establish context = () =>
         {
@@ -32,15 +32,42 @@
 
         Because of = () =>
         {
-            actual = inventory.ToString();
+            @string = inventory.ToString();
         };
 
         It should_separate_units_by_comma = () =>
         {
-            actual.Should().Be("1px1, 2px2, 5px5, 10px10, 20px20, 50px50, £1x100, £2x200, £3x300, £5x500, £10x1000, £20x2000, £50x5000");
+            @string.Should().Be("1px1, 2px2, 5px5, 10px10, 20px20, 50px50, £1x100, £2x200, £3x300, £5x500, £10x1000, £20x2000, £50x5000");
         };
 
         static Inventory inventory;
-        static string actual;
+        static string @string;
+    }
+
+    [Subject(typeof(Inventory))]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class when_copying_inventory
+    {
+        Establish context = () =>
+        {
+            origin = new Inventory
+            {
+                Pack.Of(new Pence(One), 1),
+                Pack.Of(new Pence(Two), 2)
+            };
+        };
+
+        Because of = () =>
+        {
+            copy = origin.Copy();
+        };
+
+        private It should_create_a_new_inventory = () =>
+        {
+            copy.Should().NotEqual(origin);
+        };
+
+        static Inventory origin;
+        static Inventory copy;
     }
 }
