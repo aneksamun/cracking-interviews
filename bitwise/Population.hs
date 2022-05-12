b0dfa685aed6d78e3f86c25862ee23c19f2e2370
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-05-12 23:56:46 +0100

    Implemented count population solution

---
+++ bitwise/Population.hs
@@ -0,0 +1,6 @@
+-- Counts total of '1' bit in the given number.
+-- *Main> countPopulation 109
+-- 5 (1101101)
+
+countPopulation :: Integral a => a -> Int
+countPopulation n = length $ filter (==1) $ map (`mod` 2) $ takeWhile (> 0) $ iterate (`div` 2) n
