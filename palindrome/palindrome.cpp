a097bd0a29dd6993e8f468bf1825ce64ac3e1120
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-05-18 23:17:27 +0100

    Added C++ palindrome solution

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 bool is_palindrome(const string& str) {
+    for (int i = 0; i < str.size() / 2; i++)
+        if (str[i] != str[str.size() - i - 1])
+            return false;
+    return true;
+}
+
+bool is_palindrome2(const string& str) {
     const int len = str.size() - 1;
     for (int i = 0, j = len; i <= len && j >= 0; i++, j--)
         if (str[i] != str[j])
@@ -9,7 +16,7 @@ bool is_palindrome(const string& str) {
     return true;
 }
 
-bool is_palindrome2(const string& str) {
+bool is_palindrome3(const string& str) {
     return str == string(str.rbegin(), str.rend());
 }
 
@@ -17,5 +24,5 @@ int main(int argc, const char* argv[]) {
     cout << "Enter string: ";
     string s;
     cin >> s;
-    cout << "Is palindrome: " << boolalpha << is_palindrome2(s) << endl;
+    cout << "Is palindrome: " << boolalpha << is_palindrome(s) << endl;
 }
