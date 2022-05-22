5398a7b50dadbb1d2447a73eef9156da9df2842b
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-05-22 23:36:38 +0100

    Added Scala moving average solution

---
+++ moving_average/MovingAverageCalculator.scala
@@ -0,0 +1,16 @@
+package co.uk.redpixel.category
+
+object MovingAverageCalculator {
+
+  def calculate(numbers: Seq[Int], window: Int): Array[Double] = {
+    val movingAverage = Array.ofDim[Double]((numbers.size - window) + 1)
+
+    for (i <- movingAverage.indices) {
+      val indexOfLast = i + window
+      val slice = numbers.slice(i, indexOfLast)
+      movingAverage(i) = slice.sum / slice.length.toDouble
+    }
+
+    movingAverage
+  }
+}
