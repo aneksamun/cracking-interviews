5eb5b384f1fe2aab1620c10bfd1201b1b2546a30
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-12-16 23:43:52 +0200

    Document solution

--- sum-multiples/solution.clj
+++ sum-multiples/solution.clj
@@ -1,11 +1,11 @@
 ;; (find-multiples 999 [3 5])  
 ;; (3 5 6 9 10 12 15 18 20 21 24 25 .. 999)
 
-(defn find-multiples [n xs]
-  (filter (fn [x] (some #(== (mod x %) 0) xs)) (range 1 (inc n))))
-
 ;; (sum-multiples 999 [3 5])  
 ;; 233168
+
+(defn find-multiples [n xs]
+  (filter (fn [x] (some #(== (mod x %) 0) xs)) (range 1 (inc n))))
 
 (defn sum-multiples [n xs]
   (reduce + (find-multiples n xs)))
