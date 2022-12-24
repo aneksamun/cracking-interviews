e32ce4f5bc6c916b965fbc9d1c022d01d6d2cfa6
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2022-12-24 23:13:43 +0200

    Update Solution.hs

--- problems/find-difference/max-between-increasing-nums/Solution.hs
+++ problems/find-difference/max-between-increasing-nums/Solution.hs
@@ -3,7 +3,7 @@
 --  * Finds minimal element at each position: [7, 1, 1, 1]
 --  * Combines it with given numbers: [(7,7), (1,1), (5,1), (4,1)]
 --  * Subtracts them in between: [0, 0, 4, 3]
---  * Filters zero out to respond with -1 if no difference found
+--  * Filters zeros out to respond with -1 if no difference found
 -- 
 -- Side note:
 --  zipWith (-) <*> scanl1 min 
