e373069121a59a9f3c27fb44595c2bf806134b49
Sergejs Terentjevs <sergejs.terentjevs@gmail.com>
2023-02-07 20:32:18 +0200

    Removed Max Difference Scala solution

--- problems/find-difference/max-between-increasing-nums/Solution.scala
+++
@@ -1,8 +0,0 @@
-def maximumDifference(nums: Array[Int]): Int =
-  nums.scanLeft(Int.MaxValue)(_ min _)
-    .tail
-    .zip(nums)
-    .map { case (min, elem) => elem - min }
-    .filter(_ != 0)
-    .foldLeft(-1)(_ max _)
-
